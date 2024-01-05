a306704f2a7ce006e381f8333cb75ed23e0f9a21
harsh1238 <48381596+harsh1238@users.noreply.github.com>
2024-01-05 19:28:58 +0000

    varable learning

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,8 @@
+const accountId = "12345"
+let actEmail = "h@gmail.com"
+var accrountNumber = "2345"
+
+actEmail = "h1@gmail.com"
+accrountNumber = "3456" 
+
+console.table([accountId,accrountNumber,actEmail])
